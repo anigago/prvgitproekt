8a1173cc051b38cdb2d71b717773ec80a09928e6
Anita Gagovska <anigagosh@gmail.com>
2017-12-28 21:11:39 +0100

    prv commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let ulTag = document.getElementById("lista");
+
+
+let person = {
+    name:"Anita",
+    lastname:"Gago",
+    age:34,
+    phones: ["070509509", "078836124"]
+
+}
+
+for (let i=0; i < person.phones.length; i = i+1) {
+    let newLi = document.createElement("li");
+    newLi.innerHTML = person.phones[i];
+    ulTag.appendChild(newLi);
+}
